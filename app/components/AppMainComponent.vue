6f9aa657d1e015193289c6fa583d2da0e7d73854
Michał Logaciuk <mlog00@me.pl>
2025-10-28 13:26:51 +0100

    Patch to be mobile first

--- app/components/AppMainComponent.vue
+++ app/components/AppMainComponent.vue
@@ -3,7 +3,7 @@
 <!-- h-screen overflow-auto -->
 <template>
   <!-- <div class="flex m-4 gap-4 items-center justify-evenly bg-base-100 rounded-lg"> -->
-  <div class="flex m-4 gap-4 bg-base-100 rounded-lg mx-auto">
+  <div class="flex flex-col w-full max-w-4xl mx-auto bg-base-100 rounded-lg p-2 sm:p-6">
     <slot />
   </div>
 </template>
